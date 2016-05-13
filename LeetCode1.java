d105cdb9a67a84ca23e0b333fd0b5af996f775cd
zxy <anne89757@qq.com>
2016-05-13 12:23:26 +0800

    add a new method of No.1

--- LeetCode1.java
+++ LeetCode1.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.HashMap;
 
 /*
  * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
@@ -14,7 +15,8 @@ import java.util.Arrays;
 
 public class LeetCode1 {
 public int[] twoSum(int[] nums, int target) {
-       
+   //1.solved by force
+	/*
 	 for(int i=0;i<nums.length-1;i++){
     	 for(int j=i+1;j<nums.length;j++){
     		 if(nums[i]+nums[j]==target){
@@ -25,7 +27,21 @@ public int[] twoSum(int[] nums, int target) {
     	 }
     }
     return null;
-  
+  */
+	
+    //2.use hashmap
+	HashMap map=new HashMap();
+	for(int i=0;i<nums.length;i++){
+		map.put(nums[i],i);    //input num as index, i as value
+	}
+	for(int j=0;j<nums.length;j++){
+		int gap=target-nums[j];
+		if(map.get(gap)!=null&&(int)map.get(gap)!=j){
+			int [] r={j,(int) map.get(gap)};
+			return r;
+		}
+	}
+	return null;
     }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
